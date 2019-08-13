e3883c3fa809d00803c3fd0a21eb68fd883a62e8
Brett Esler <brett.esler@oridashi.com.au>
2019-08-13 19:29:26 +1000

    Create buildv.bat

---
+++ buildv.bat
@@ -0,0 +1,8 @@
+@echo off 
+
+echo You have to edit the path to the publisher...
+pause
+
+"C:\Program Files\Java\jre1.8.0_211\bin\java.exe" -Xmx2048m -jar "org.hl7.fhir.publisher.jar" -ig ig.json -publish http://hl7.org.au/fhir/pd/2019Aug
+
+pause
